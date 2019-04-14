ba935306f861ef87606d1d8911e2e5a72ccf5bdd
lutz <lutzfinke2010@gmail.com>
2019-04-14 18:53:19 +0200

    unnötige Klassen entfernt

--- src/main/java/de/maxya/inventorytrouble/control/login/WarteRaumException.java
+++
@@ -1,8 +0,0 @@
-package de.maxya.inventorytrouble.control.login;
-
-public class WarteRaumException extends Throwable {
-    @Override
-    public String getMessage() {
-        return "Warteraum";
-    }
-}
